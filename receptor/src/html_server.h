f64c922b64719504b4df2e202494d86bb301bd45
amarildo lacerda <amarildo_51@msn.com>
2025-05-26 17:38:50 -0300

    - removido codigo nao usado

--- receptor/src/html_server.h
+++
@@ -1,8 +0,0 @@
-#ifndef HTML_SERVER_H
-#define HTML_SERVER_H
-#ifdef ESP8266
-
-void initHtmlServer();
-void processHtmlServer();
-#endif
-#endif
--- receptor/src/v1.h
+++
@@ -1,9 +0,0 @@
-#ifdef OLD
-#ifndef RECEPTOR_H
-#define RECEPTOR_H
-
-// void setup();
-// void loop();
-
-#endif // RECEPTOR_H
-#endif
